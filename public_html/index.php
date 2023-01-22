2733d774b6b1d2e5b03b7b84404a09a2986b1255
knot126 <73200930+knot126@users.noreply.github.com>
2023-01-22 23:31:19 +0000

    Logout and starting work on mod pages

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,7 @@
 
 require_once "../data/page.php";
 require_once "../data/action.php";
+require_once "../data/mod.php";
 
 function main() {
 	if (array_key_exists("a", $_GET)) {
@@ -12,16 +13,23 @@ function main() {
 			case "login":
 				do_login();
 				break;
+			case "logout":
+				do_logout();
+				break;
 			default:
 				break;
 		}
 	}
+	else if (array_key_exists("m", $_GET)) {
+		include_header();
+		display_mod($_GET["m"]);
+		include_footer();
+	}
 	else {
 		include_header();
+		include_static_page();
+		include_footer();
 	}
-	
-	include_static_page();
-	include_footer();
 }
 
 main();
