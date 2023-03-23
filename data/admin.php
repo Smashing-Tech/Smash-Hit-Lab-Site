e28f6c115470bd3dc1d1f8f875643f09f1ce18d1
knot126 <73200930+knot126@users.noreply.github.com>
2023-03-23 21:58:22 +0000

    Adding mode where only verified users can log in

--- data/admin.php
+++ data/admin.php
@@ -23,7 +23,7 @@ function do_site_config() {
 			echo "<form action=\"./?a=site_config&submit=1\" method=\"post\">";
 			edit_feild("enable_discussions", "select", "Discussions", "If discussions should be enabled, disabled or closed sitewide. Closed will disable new comments but still show old ones, while disabled will stop showing them entirely. Comments can still be marked as hidden when closed, but cannot when disabled.", get_config("enable_discussions", "enabled"), true, array("enabled" => "Enabled", "disabled" => "Disabled", "closed" => "Closed"));
 			edit_feild("register", "select", "Enable registering", "Weather registering of new accounts should be limited or not.", get_config("register", "anyone"), true, array("anyone" => "Anyone can register", "users" => "Only users can register", "admins" => "Only admins can register", "closed" => "Registering is disabled"));
-			edit_feild("enable_login", "select", "Enable logins", "Allow users to log in to the stie.</p><p><b>Warning:</b> If you set this to completely disabled and all admins are logged out, then you need to wait for Knot126 to fix the site.", get_config("enable_login", "users"), true, array("users" => "All users can log in", "admins" => "Only admins can log in", "closed" => "Logging in is disabled"));
+			edit_feild("enable_login", "select", "Enable logins", "Allow users to log in to the stie.</p><p><b>Warning:</b> If you set this to completely disabled and all admins are logged out, then you need to wait for Knot126 to fix the site.", get_config("enable_login", "users"), true, array("users" => "All users can log in", "verified" => "Verified users and admins can log in", "admins" => "Only admins can log in", "closed" => "Logging in is disabled"));
 			echo "<input type=\"submit\" value=\"Save settings\"/>";
 			echo "</form>";
 			include_footer();
@@ -31,7 +31,7 @@ function do_site_config() {
 		else {
 			set_config("enable_discussions", $_POST["enable_discussions"], array("enabled", "disabled", "closed"));
 			set_config("register", $_POST["register"], array("anyone", "users", "admins", "closed"));
-			set_config("enable_login", $_POST["enable_login"], array("users", "admins", "closed"));
+			set_config("enable_login", $_POST["enable_login"], array("users", "verified", "admins", "closed"));
 			alert("Site config was updated by $user", "./?a=site_config");
 			redirect("./?a=site_config");
 		}
